33606a77c1324f4cc8dfccb263c1316aaf8eb4c8
RediiChen <a0929052958@gmail.com>
2021-03-29 12:20:52 +0800

    門診時間強調機制建立

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 //* 全域共用區
 const isMobile = window.matchMedia("(max-width: 767px)");
-    //TODO 待研究這個變數到底是什麼
+//TODO 待研究這個變數到底是什麼
 // 全域共用區 END
 //* 滾動機制 ==========================//
 //功能：藉由jQuery實現捲動前往指定位置
@@ -47,7 +47,7 @@ function showCard(n) {
     var i;//for 迴圈用的這個變數不會被關在for裡頭，而會被提昇到這裡。
     var cards = document.getElementsByClassName("team-card");
     var btns = document.getElementsByClassName("team-btn");
-    
+
 
     //全部卡片隱藏，全部按鈕還原
     for (i = 0; i < cards.length; i++) {
@@ -85,33 +85,59 @@ $("#care-board-3").slick(
 //* 門診頁面 I : 表格單色化 ==========================//
 // var egBtns = document.getElementsByClassName("table-eg-btns") ;
 $(".table-eg-btns").each(function () {
-    var color = this.attr("data-tt-label");
-    if (!color) return false;// 沒有設定此參數的就直接中止
-    
-    //! 以下功能沒有成功
+    var color = $(this).attr("data-tt-label");
+    if (color === undefined) return true;// 沒有設定此參數的就直接中止
+    console.log("抓到了 " + color);
     if (isMobile.matches) {
-        $(this).mousedown(addGray(color)) ;
-        $(this).mouseup(removeGray()) ;
+        $(this).mousedown(addGray(color));
+        $(this).mouseup(removeGray());
+        console.log("行動裝置");
     } else {
-        $(this).hover(addGray(color), removeGray());
+        //* 以下的是會失敗的外包版本
+        // $(this).hover(addGray(color), removeGray());
+        console.log("電腦");
+        //* 以下的是會成功的嵌入版本
+        $(this).hover(
+            function () {
+                var i;
+                var cols = document.getElementsByClassName("tt-cols");
+                for (i = 0; i < cols.length; i++) {
+                    if (!cols[i].classList.contains(color)) {
+                        cols[i].classList.add("tt-deactivate");
+                    }
+                }
+            },
+            function () {
+                var i;
+                var cols = document.getElementsByClassName("tt-cols");
+                for (i = 0; i < cols.length; i++) {
+                    cols[i].classList.remove("tt-deactivate");
+                }
+            }
+        );
     }
 });
-
+//* 以下的是會失敗的外包版本
 function addGray(colorLabel) {
-    var i;//for 迴圈用的這個變數不會被關在for裡頭，而會被提昇到這裡。
+    console.log("進來了！");
+    console.log("現在的顏色是 " + colorLabel);
+
+    var i;
     var cols = document.getElementsByClassName("tt-cols");
     for (i = 0; i < cols.length; i++) {
-        if (cols.classList.contains(colorLabel)) {
-            cols.classList.add("tt-deactivate");
+        if (!cols[i].classList.contains(colorLabel)) {
+            cols[i].classList.add("tt-deactivate");
+            // console.log("add i = " + i);
         }
     }
 }
 function removeGray() {
-    var i;//for 迴圈用的這個變數不會被關在for裡頭，而會被提昇到這裡。
+    console.log("離開了！");
+
+    var i;
     var cols = document.getElementsByClassName("tt-cols");
-    if (!color) return false;// 沒有設定此參數的就直接中止
     for (i = 0; i < cols.length; i++) {
-        cols.classList.remove("tt-deactivate");
+        cols[i].classList.remove("tt-deactivate");
     }
 }
 
