3480ae6f104ecf98f20839f0f689a49e28f9547d
RediiChen <a0929052958@gmail.com>
2021-03-29 20:30:50 +0800

    last update

--- history/history.js
+++ history/history.js
@@ -1,7 +1,15 @@
-$("news-bars").each(function (index, bars) {
-    var bricks = $(".news-bricks") ;
-    console.log("知道了 " + bricks.length) ;
-    bars[index].click(function () {
-        bricks[index].slideToggle() ;
-    }) ;
-}) ;
+// $("news-bars").each(function (index, bars) {
+//     var bricks = $(".news-bricks") ;
+//     console.log("知道了 " + bricks.length) ;
+//     bars[index].click(function () {
+//         bricks[index].slideToggle() ;
+//     }) ;
+// }) ;
+
+for ( let i = 0 ; i < 5 ; i++) {
+    var bars = document.getElementsByClassName(".news-bars") ;
+    var bricks = document.getElementsByClassName(".news-bricks") ;
+    bars[i].click(function () {
+        $bricks[i].slideToggle("normal") ;
+    })
+}
